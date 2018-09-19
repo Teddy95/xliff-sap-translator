5b519cd170e4a80b8313c1cd6f2711b1fd2d2db0
Andre Sieverding <webmaster@andre-sieverding.de>
2018-09-19 12:45:03 +0200

    Add files via upload

---
+++ sandbox_api.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Author: Andre Sieverding
+ * Date: 05.09.2018
+ */
+
+// Deactivate error reporting
+error_reporting(0);
+ini_set('display_errors', 0);
+
+// Set HTTP header to UTF-8
+header('Content-Type: text/html; charset=utf-8');
+
+$JSON->translations = array();
+$JSON->translations[0]->text = $_GET['string'];
+echo json_encode($JSON);
